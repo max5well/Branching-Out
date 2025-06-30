daaed8c1653af2c68100c6b72bc27ecf153f1ee2
max5well <maximiliandev5@gmail.com>
2025-06-30 17:47:08 +0200

    Added new feature: filter by email

--- filter_users.py
+++ filter_users.py
@@ -25,13 +25,29 @@ def filter_users_by_age():
     for user in filtered_users:
         print(user)
 
+def filter_users_by_email():
+    try:
+        mail_to_search = input("Enter an email to filter users: ").strip()
+    except ValueError:
+        print("Invalid input. Please enter an email.")
+        return
+
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"] == mail_to_search]
+    for user in filtered_users:
+        print(user)
+
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, 'name' or 'age' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, 'name', 'email' or 'age' is supported): ").strip().lower()
 
     if filter_option == "name":
         filter_users_by_name()
     elif filter_option == "age":
         filter_users_by_age()
+    elif filter_option == "email":
+        filter_users_by_email()
     else:
         print("Filtering by that option is not yet supported.")
